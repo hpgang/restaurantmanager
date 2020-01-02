a0a23a62bc7ce5f0a051a30ab8704a0c1ecbfa4a
Helge Peel <lbsprojekt2019@gmail.com>
2020-01-02 15:19:38 +0100

    Tischreservierung in Huge einbinden

---
+++ html/application/view/tischreservierung/index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="de" >
+<head>
+  <meta charset="UTF-8">
+  <title>Table Plan</title>
+  <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/tischreservierung/tischreservierung.css" />
+</head>
+<body>
+<table class="tisch1">
+  <tr>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+  </tr>
+  <tr>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch4" colspan="4" rowspan="2" class="tisch">Tisch 4</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch6" colspan="4" rowspan="2" class="tisch">Tisch 6</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+  </tr>
+  <tr>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch1" colspan="2" class="tisch">Tisch 1</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch 2" colspan="2" class="tisch">Tisch 2</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch3" colspan="2" class="tisch">Tsich 3</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch5" colspan="2" class="tisch">Tisch 5</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch7" colspan="2" class="tisch" >Tisch 7</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch8" colspan="2" class="tisch">Tisch 8</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch9" colspan="2" class="tisch">Tisch 9</td>
+
+  </tr>
+  <tr>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    
+  </tr>
+  <tr>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch10" colspan="2" class="tisch">Tisch 10</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch12" colspan="2" rowspan="2" class="tisch">Tisch 12</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch13" colspan="2" rowspan="2" class="tisch">Tisch 13</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="res" id="bar" colspan="10" rowspan="2">Bar</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    
+    <td id="tisch14" colspan="2" rowspan="2" class="tisch">Tisch 14</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch 15" colspan="2" rowspan="2"class="tisch">Tisch 15</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch16" colspan="2" class="tisch">Tisch 16</td>
+    <td class="inv"></td>
+  </tr>
+  <tr>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="tisch11" class="tisch" colspan="2" class="tisch">Tisch 11</td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td id="Tisch17" colspan="2" class="tisch">Tisch 17</td>
+  </tr>
+  <tr>
+    <td class="inv"></td> 
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+  </tr>
+  <tr>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+    <td class="inv"></td>
+  </tr>
+</table>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+  <script>
+  $( ".tisch" ).click(function() {
+    $(this).toggleClass("reserviert");
+});
+  </script>
+</body>
+</html>
